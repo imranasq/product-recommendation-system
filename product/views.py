42cf71d5a6a9d8c5106f736f2eb504c8626fa818
imranasq <tech.alimran@gmail.com>
2022-09-24 15:33:44 +0600

    Category viewset added

--- product/views.py
+++ product/views.py
@@ -1,10 +1,16 @@
 from rest_framework import viewsets
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Product
+from .models import Product, Category
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
