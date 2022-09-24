aea1b72c71f0420304b00b46079b199d77018604
imranasq <tech.alimran@gmail.com>
2022-09-24 07:52:49 +0600

    users default functionalities added

---
+++ user/serializers.py
@@ -0,0 +1,63 @@
+from .models import User
+from rest_framework import serializers
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(style={"input_type": "password"}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'user_type', 'password', 'password2']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def save(self):
+        user = User(email=self.validated_data['email'], user_type=self.validated_data['user_type'])
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        if password != password2:
+            raise serializers.ValidationError({'password': 'Passwords must match.'})
+        user.set_password(password)
+        user.is_active = True
+        if self.validated_data["user_type"] == "Admin":
+            user.is_admin = True
+            user.is_superuser = True
+            user.is_staff = True
+        user.save()
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = [
+            'id',
+            'first_name',
+            'last_name',
+            'email',
+            'password',
+            'user_type',
+            'is_active',
+        ]
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.is_active=True
+        if validated_data["user_type"] == "Admin":
+            user.is_superuser = True
+            user.is_staff = True
+        user.save()
+        return user
+
+class PasswordChangeSerializer(serializers.Serializer):
+    current_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
+    new_password2 = serializers.CharField(required=True)
+
+    def validate_current_password(self, value):
+        if not self.context['request'].user.check_password(value):
+            raise serializers.ValidationError({'current_password': 'Does not match'})
+        return value
